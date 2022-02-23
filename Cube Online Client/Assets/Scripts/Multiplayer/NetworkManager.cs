16e04ba0907d228ebed15a6e242addb01553a525
oskari83 <oskpelto@gmail.com>
2022-02-23 15:17:45 +0200

    Initial commit

---
+++ Cube Online Client/Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using RiptideNetworking;
+using RiptideNetworking.Utils;
+using UnityEngine;
+
+public enum ClientToServerId : ushort{
+    name = 1,
+}
+public class NetworkManager : MonoBehaviour
+{
+    private static NetworkManager _singleton;
+
+    public static NetworkManager Singleton{
+        get => _singleton;
+        
+        private set{
+            if(_singleton == null)
+                _singleton = value;
+            else if (_singleton != value){
+                Debug.Log($"{nameof(NetworkManager)} instance already exists, destroying duplicate!");
+                Destroy(value);
+            }
+        }
+    }
+
+    public Client Client { get; private set; }
+
+    [SerializeField] private string ip;
+    [SerializeField] private ushort port;
+
+    private void Awake(){
+        Singleton = this;
+    }
+
+    private void Start(){
+        RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
+        Client = new Client();
+        Client.Connected += DidConnect;
+        Client.ConnectionFailed += FailedToConnect;
+        Client.Disconnected += DidDisconnect;
+    }
+
+    private void FixedUpdate(){
+        Client.Tick();
+    }
+
+    private void OnApplicationQuit(){
+        Client.Disconnect();
+    }
+
+    public void Connect(){
+        Client.Connect($"{ip}:{port}");
+    }
+
+    private void DidConnect(object sender, EventArgs e){
+        UIManager.Singleton.SendName();
+    }
+
+    private void FailedToConnect(object sender, EventArgs e){
+        UIManager.Singleton.BackToMain();
+    }
+
+    private void DidDisconnect(object sender, EventArgs e){
+        UIManager.Singleton.BackToMain();
+    }
+}
