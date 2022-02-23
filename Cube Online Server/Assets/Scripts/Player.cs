427780359a304bb0ac8e401133a563bc90223e6f
oskari83 <oskpelto@gmail.com>
2022-02-23 16:23:43 +0200

    Added Server

---
+++ Cube Online Server/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using RiptideNetworking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Dictionary<ushort, Player> list = new Dictionary<ushort, Player>();
+
+    public ushort Id {get; private set; }
+    public string Username {get; private set;}
+
+    private void OnDestroy(){
+        list.Remove(Id);
+    }
+    public static void Spawn(ushort id, string username){
+        Player player = Instantiate(GameLogic.Singleton.PlayerPrefab, new Vector3(0f,1f,0f), Quaternion.identity).GetComponent<Player>();
+        player.name = $"Player {id} ({(string.IsNullOrEmpty(username) ? "Guest" : username)}";
+        player.Id = id;
+        player.Username = string.IsNullOrEmpty(username) ? $"Guest {id}" : username;
+        list.Add(id, player);
+    }
+
+    [MessageHandler((ushort)ClientToServerId.name)]
+    private static void Name(ushort fromClientId, Message message){
+        Spawn(fromClientId, message.GetString());
+    }
+}
